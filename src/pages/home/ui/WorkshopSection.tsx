b19052e095a062673b946e6f262752b11f5779e8
optshj <optshj@gmail.com>
2026-01-31 22:24:02 +0900

    ✨ [Feat] 메인페이지 API호출 시 Suspnse 및 에러처리

--- src/pages/home/ui/WorkshopSection.tsx
+++ src/pages/home/ui/WorkshopSection.tsx
@@ -1,10 +1,9 @@
 'use client';
-import Link from 'next/link';
 import Image from 'next/image';
 import { Carousel, SectionDetailTitle } from './Components';
-import { MoveRight } from 'lucide-react';
 import myImage from 'shared/image/image.jpg';
 import { motion } from 'motion/react';
+import { AsyncBoundary } from 'shared/error/AsyncBoundary';
 
 interface WorkshopData {
   title: string;
@@ -32,22 +31,27 @@ export const WorkshopSection = () => {
           일할 땐 열정적으로, 놀 땐 누구보다 진심으로!
         </motion.p>
       </div>
-      <Carousel data={data} renderItem={(item) => <Item data={item} />} className="gap-3 sm:gap-11.5" />
+      <AsyncBoundary>
+        <WorkshopCarousel />
+      </AsyncBoundary>
     </section>
   );
 };
 
-const Item = ({ data }: { data: WorkshopData }) => {
+const WorkshopCarousel = () => {
   return (
-    <div className="group relative h-30 w-44 cursor-pointer overflow-hidden rounded-sm bg-gray-900 sm:h-95 sm:w-171.5 sm:rounded-xl">
-      <Image draggable={false} loading="lazy" src={myImage} alt={data.title} fill className="object-cover" />
-      <Link draggable={false} href={`/workshop/id=${data.title}`} className="absolute inset-0 z-10">
-        <div className="bg-background-surface/80 absolute inset-0 flex flex-col items-start justify-start gap-0.5 px-3 py-2 text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 sm:gap-2 sm:px-6.75 sm:py-3.25">
-          <span className="text-primary-gradient text-[9px] sm:text-[36px]">{data.title}</span>
-          <span className="text-custom-gray-500 text-[4.5px] sm:text-lg">{data.date}</span>
-          <MoveRight className="text-custom-gray-500 animate-wiggle-right absolute right-4 bottom-1 w-4 sm:right-6 sm:bottom-6 sm:w-10" />
+    <Carousel
+      data={data}
+      className="gap-3 sm:gap-11.5"
+      renderItem={(item) => (
+        <div className="group relative h-30 w-44 overflow-hidden rounded-sm bg-gray-900 sm:h-95 sm:w-171.5 sm:rounded-xl">
+          <Image draggable={false} loading="lazy" src={myImage} alt={item.title} fill className="object-cover" />
+          <div className="bg-background-surface/80 absolute inset-0 flex flex-col items-start justify-start gap-0.5 px-3 py-2 text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 sm:gap-2 sm:px-6.75 sm:py-3.25">
+            <span className="text-primary-gradient text-[9px] sm:text-[36px]">{item.title}</span>
+            <span className="text-custom-gray-500 text-[4.5px] sm:text-lg">{item.date}</span>
+          </div>
         </div>
-      </Link>
-    </div>
+      )}
+    />
   );
 };
